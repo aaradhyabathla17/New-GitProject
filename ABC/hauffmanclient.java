62ab6517adf751d20536031a3523fcc1e1c37388
Anjali <aaradhyab001@gmail.com>
2019-08-19 01:41:11 -0700

    xyz

---
+++ ABC/hauffmanclient.java
@@ -0,0 +1,13 @@
+package crux7april;
+
+public class hauffmanclient {
+
+	public static void main(String[] args) {
+		String str="aaaabcddddd";
+		hauffmann hc=new hauffmann(str);
+		System.out.println(hc.encode("abcdddd"));
+		System.out.println(hc.decode("111001010000"));
+
+	}
+
+}
